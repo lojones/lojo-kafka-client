3a59fabc32518b195b61e937c9f399d9eafb8916
Login Jones <login.jones@gmail.com>
2017-02-12 10:14:46 -0500

    First Commit

---
+++ src/main/java/com/lojo/example/kafka/LojoKafkaClientApplication.java
@@ -0,0 +1,12 @@
+package com.lojo.example.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojoKafkaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojoKafkaClientApplication.class, args);
+	}
+}
